02255bd29fcfb3cfd3ccdf6ff27aa10ff8051491
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-05-21 12:21:17 +0200

    gears model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_100042) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_101535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gears", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "hourly_rate"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_gears_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_21_100042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "gears", "users"
 end
