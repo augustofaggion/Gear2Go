7b0d0226c84dc8cbfbea570787939548cffbf9a6
Solros <60941951+Solrosy@users.noreply.github.com>
2024-05-22 00:56:21 +0200

    added ability to manage own gears, when business_name is set for user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   # Ensure the user is authenticated - we could also do this in the menu...
   before_action :authenticate_user!
+  
 
   def show
     if current_user.business_name.present?
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root to: 'gears#index'
   devise_for :users
+  delete '/dashboard/gears/:id', to: 'dashboard/gears#destroy', as: 'dashboard_gear'
 
   resources :gears, only: [:index, :show] do
     resources :bookings, only: [:create, :show]
   end
 
-  root to: 'gears#index'
+  namespace :dashboard do
+    resources :gears, except: [:show]
+  end
+
   get 'dashboard', to: 'dashboard#show'
+
+
+
 end
