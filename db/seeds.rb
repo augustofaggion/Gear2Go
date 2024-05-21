87ffce129059ddd3410860c0dd7a655f4cdcae0c
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-05-21 14:09:53 +0200

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# First user a@wagon.com password: a
+user_gear1 = Gear.new(name: "Surfboard", description: "A surfboard", hourly_rate: 10, user_id: 1)
+user_gear1.save
+
+user_gear2 = Gear.new(name: "Jetski", description: "Jetski for who like adventure", hourly_rate: 50, user_id: 1)
+user_gear2.save
+
+user_gear3 = Gear.new(name: "Beach umbrella", description: "Good for big families", hourly_rate: 5, user_id: 1)
+user_gear3.save
+
+# Second user
+user_account2 = User.new(email: "b@wagon.com", password: "b")
+user_account2.save
+
+user2_gear1 = Gear.new(name: "Boat", description: "A Boat", hourly_rate: 100, user_id: 1)
+user2_gear1.save
+
+user2_gear2 = Gear.new(name: "Windboard", description: "Windboard for who like adventure", hourly_rate: 50, user_id: 1)
+user2_gear2.save
+
+user2_gear3 = Gear.new(name: "Beach umbrella", description: "Good for big families", hourly_rate: 5, user_id: 1)
+user2_gear3.save
+
+# Third user
+user_account3 = User.new(email: "c@wagon.com", password: "c")
+user_account3.save
+
+user3_gear1 = Gear.new(name: "Submarine", description: "A Submarine", hourly_rate: 500, user_id: 1)
+user3_gear1.save
+
+user3_gear2 = Gear.new(name: "Jetski", description: "Jetski for who like adventure", hourly_rate: 50, user_id: 1)
+user3_gear2.save
+
+user3_gear3 = Gear.new(name: "Beach umbrella", description: "Good for big families", hourly_rate: 5, user_id: 1)
+user3_gear3.save
