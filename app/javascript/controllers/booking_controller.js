556c9628ba7877d723e0b790726d57de687844a9
Sonja Lorscheidt <sonja.lorscheidt@gmail.com>
2024-05-22 17:26:34 +0200

    Finished time selection

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -5,6 +5,24 @@ export default class extends Controller {
   static targets = [ "date", "timeSelect" ]
   static values = { slots: Array }
 
+  connect() {
+    // Set the first date button to active
+    this.dateTargets[0].classList.add("active")
+    this.dateTargets[0].classList.add("btn-info")
+
+    // Prefill the select with the slots for the first date
+    let slots = this.slotsValue
+    this.timeSelectTarget.innerHTML = ""
+
+    slots[0].slots.forEach(slot => {
+      let option = document.createElement("option")
+      let formatted_slot = slot.substring(11,16)
+      option.innerText = formatted_slot
+      option.value = slot
+      this.timeSelectTarget.add(option)
+    })
+  }
+
   switchDate(event) {
     document.querySelectorAll(".active").forEach((date) => {
       date.classList.remove("active")
