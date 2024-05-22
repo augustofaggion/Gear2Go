8878cb6ddc5d3301c403df8bbccb8e2f2bcec087
Sonja Lorscheidt <sonja.lorscheidt@gmail.com>
2024-05-22 12:06:40 +0200

    Available bookings slots method added

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,4 +1,10 @@
 class Gear < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  def available_slots(date)
+    booked_slots = bookings.where(start_time: date.beginning_of_day..date.end_of_day).pluck(:start_time)
+    all_slots = (9..16).map { |hour| date.to_time.change(hour: hour) }
+    all_slots - booked_slots
+  end
 end
