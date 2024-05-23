3d9fb6674ad9f2cca400f1d139809d108c504b44
Sonja Lorscheidt <sonja.lorscheidt@gmail.com>
2024-05-23 10:38:22 +0200

    Search added + cards refined

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -3,14 +3,16 @@ class GearsController < ApplicationController
   before_action :set_gear, only: [:show]
 
   def index
-    @gears = Gear.all
+    @gears = Gear.joins(:user)
+
+    if params[:query].present?
+      @gears = @gears.where("users.city ILIKE ?", "%#{params[:query]}%")
+    end
   end
 
   def show
     @booking = Booking.new
     @gear = Gear.find(params[:id])
-    # date = Date.today
-    # @available_slots = @gear.available_slots(date)
     @next_7_days_with_slots = @gear.next_7_days_with_slots
   end
 
