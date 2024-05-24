a3e6e727fab57bf73ed85ae29a6e243d78255dbd
Sonja Lorscheidt <sonja.lorscheidt@gmail.com>
2024-05-24 11:57:55 +0200

    Added comments to gear

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -6,8 +6,11 @@ class Gear < ApplicationRecord
   # Calculates the available booking slots for a given date
   # Assumes that all gear is bookable from 9am to 5pm.
   def available_slots_for_day(date)
+    # Creates an array of all the booked slots for the given day
     booked_slots = bookings.where(start_time: date.beginning_of_day..date.end_of_day).pluck(:start_time)
+    # Creates an array of all the possible slots for the given day
     all_slots = (9..17).map { |hour| date.to_time.change(hour: hour) }
+    # Returns the difference between all_slots and booked_slots which are the available slots
     all_slots - booked_slots
   end
 
@@ -16,6 +19,7 @@ class Gear < ApplicationRecord
     (0..n-1).map do |i|
       date = Date.today + i.days
       available_slots = self.available_slots_for_day(date)
+      # Generates a hash with the date and the available slots for that day
       { date: date, slots: available_slots }
     end
   end
